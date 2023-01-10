71f91de355c696e6f97f3715d0ba032fe3f1d8d6
AndreySkripachev <andrey-skripachev@saritasa.com>
2023-01-10 23:38:29 +0700

    Fixes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,19 @@
 const http = require('http');
+var os = require('os');
+var ifaces = os.networkInterfaces();
+let ipList = [];
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      return;
+    }
+
+    ipList.push(iface.address);
+  });
+});
 
 const host = 'localhost';
 const port = 8800;
@@ -59,6 +74,6 @@ const requestListener = function (req, res) {
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, '172.17.0.2', () => {
+server.listen(port, ipList[0], () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
